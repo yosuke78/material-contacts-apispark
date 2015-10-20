1027f724f5f33b8b4175d1f8cbca47cca0432a5c
Manuel Boillod <boillodmanuel@gmail.com>
2015-10-20 02:19:07 +0200

    fix invlid import

--- src/main/java/net/apispark/webapi/representation/Contact.java
+++ src/main/java/net/apispark/webapi/representation/Contact.java
@@ -2,7 +2,6 @@ package net.apispark.webapi.representation;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.sun.tools.javac.jvm.Gen;
 import net.apispark.webapi.core.validation.ValidationErrors;
 import net.apispark.webapi.representation.enums.Gender;
 
